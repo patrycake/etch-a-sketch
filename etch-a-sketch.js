9ab3025d0af1657275ca707f6947ef7e78f313fe
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-11-09 09:28:58 -0500

    add mouseover eventlistener

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -14,4 +14,18 @@ function createEtchBoard(num) {
     }
 }
 
+function createEventListenerOnCol(functionName, num) {
+    for (let row = 0; row < num; row++) {
+        for (let col = 0; col < num; col++) {
+            let divCol = document.getElementById(`row${row}col${col}`);
+            divCol.addEventListener("mouseover", mouseOverNormal);
+        }
+    }
+}
+
+function mouseOverNormal() {
+    this.style.backgroundColor = "#C7866A"
+}
+
 createEtchBoard(16);
+createEventListenerOnCol(mouseOverNormal, 16);
